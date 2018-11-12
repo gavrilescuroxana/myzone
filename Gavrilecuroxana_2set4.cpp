35f1777fb7c0842784c99f6a76fef47ea2cb80b2
gavrilescuroxana <44976357+gavrilescuroxana@users.noreply.github.com>
2018-11-12 17:36:23 +0200

    Add files via upload

---
+++ Gavrilecuroxana_2set4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int a;
+int b;
+int main()
+{
+  cout<<"a="; cin>>a;
+  cout<<"b="; cin>>b;
+  if(a=b){cout<<"a";}
+  else{
+   while(a!=b){
+
+      if(a>b) {cout<<a,a-b}
+        else(b<a){cout<<b, b-a}
+
+        if((a,b)=1){cout<<"Da";
+        else{ cout<<"NU";}
+
+
+     return 0;
+   }
+}
